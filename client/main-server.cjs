7697fe3a200c2b5c3eee4147ed9416f66adb15e2
Your Name <your.email@example.com>
2025-07-27 16:42:11 +0000

    test: add back catch-all * route

--- client/main-server.cjs
+++ client/main-server.cjs
@@ -16,6 +16,11 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
+console.log('Registering route: *');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 }); 
