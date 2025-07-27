cdbc041a1f897c5602590d53b7d6f9fcb1e5f70f
Your Name <your.email@example.com>
2025-07-27 16:35:18 +0000

    test: add back static middleware only

--- client/main-server.cjs
+++ client/main-server.cjs
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5002;
+
+console.log('Registering static middleware:', path.join(__dirname, 'dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 console.log('Registering route: /');
 app.get('/', (req, res) => {
