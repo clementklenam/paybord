7c22e55bbbad3fead42b70196cd04e9c96c02aad
Your Name <your.email@example.com>
2025-07-27 16:38:56 +0000

    test: add back /pl/:id route

--- client/main-server.cjs
+++ client/main-server.cjs
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 5002;
 console.log('Registering static middleware:', path.join(__dirname, 'dist'));
 app.use(express.static(path.join(__dirname, 'dist')));
 
+console.log('Registering route: /pl/:id');
+app.get('/pl/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 console.log('Registering route: /');
 app.get('/', (req, res) => {
   res.send('Hello, world!');
